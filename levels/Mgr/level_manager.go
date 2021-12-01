2c699080e3caea953d80e54a7d53914ea5f19f2e
HIKOkyoujurou <73946556+HIKOkyoujurou@users.noreply.github.com>
2021-12-01 15:39:43 +0900

    stream

--- levels/Mgr/level_manager.go
+++ levels/Mgr/level_manager.go
@@ -49,3 +49,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "player_factory"
+  type: "collectionfactory"
+  data: "prototype: \"/levels/actor/player/player.collection\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
